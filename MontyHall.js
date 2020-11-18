039ace761eeba22bb5b95c234aac346cfd64e740
Kyle Barker <kb33@usca.edu>
2020-11-18 00:07:44 -0500

    jquery test

---
+++ MontyHall.js
@@ -0,0 +1,12 @@
+$('door1').click(function(){
+    $('.selected').removeClass('selected'); // removes the previous selected class
+    $(this).addClass('selected'); // adds the class to the clicked image
+    });
+$('door2').click(function(){
+    $('.selected').removeClass('selected'); // removes the previous selected class
+    $(this).addClass('selected'); // adds the class to the clicked image
+    });
+$('door3').click(function(){
+    $('.selected').removeClass('selected'); // removes the previous selected class
+    $(this).addClass('selected'); // adds the class to the clicked image
+    });

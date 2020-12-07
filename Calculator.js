fbc63671ab37094fbad6e97213f41f57806451e3
Kyle Barker <kb33@usca.edu>
2020-12-07 15:15:58 -0500

    commit

--- Calculator.js
+++ Calculator.js
@@ -106,6 +106,7 @@ $('#divide').click(function(){
 
 $('#power').click(function(){
     $("#displayNumber").text("^");
+    //commit
     numList.push(numListValue);
     if (operList.length != 0) {
         docalculation();

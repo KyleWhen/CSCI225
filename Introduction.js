f927db1045d61942e0aff0d7cd8baa40ae48f6fd
Kyle Barker <kb33@usca.edu>
2020-12-07 12:35:44 -0500

    commit

--- Introduction.js
+++ Introduction.js
@@ -1,20 +1,30 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyAQ3-DuFMINqU412qUeua5WMZZPU1l1EuU",
-    authDomain: "grocerylist-bde77.firebaseapp.com",
-    databaseURL: "https://grocerylist-bde77.firebaseio.com",
-    projectId: "grocerylist-bde77",
-    storageBucket: "grocerylist-bde77.appspot.com",
-    messagingSenderId: "139163594125",
-    appId: "1:139163594125:web:36edde353ef2c80e4861c4"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  apiKey: "AIzaSyAQ3-DuFMINqU412qUeua5WMZZPU1l1EuU",
+  authDomain: "grocerylist-bde77.firebaseapp.com",
+  databaseURL: "https://grocerylist-bde77.firebaseio.com",
+  projectId: "grocerylist-bde77",
+  storageBucket: "grocerylist-bde77.appspot.com",
+  messagingSenderId: "139163594125",
+  appId: "1:139163594125:web:36edde353ef2c80e4861c4"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+
+$(".rating input[type='submit']").click(function (e) {
+  var inputdata = $("form").serializeArray();
+  console.log(inputdata);
+  var inputJson = {};
+  for (var i = 0; i < inputdata.length; i++) {
+    var name = inputdata[i]["name"];
+    var value = inputdata[i]["value"];
+    console.log(name + " " + value);
+    inputJson[name] = value;
+  }
 
   firebase
     .firestore()
     .collection("feedback")
     .add(inputJson);
-
-    $(".rating input[type='submit']").click(function(e){
-        e.preventDefault();
-      });
+});

0841d4c4a952886990912da3480875be4684b736
Kyle Barker <kb33@usca.edu>
2021-03-28 22:58:16 -0400

    test

--- Translator.js
+++ Translator.js
@@ -5,19 +5,19 @@ var operList = [];
 var operand = ["",""];
 var numListValue = 0;
 
-$('#four').click(function(){
+$('#Sinister').click(function(){
     $("#displayNumber").text("Left");
     numListValue = (numListValue * 10) + 4;
     $('#displayNumber').text("Left");
 });
 
-$('#five').click(function(){
+$('#Medium').click(function(){
     $("#displayNumber").text("Middle");
     numListValue = (numListValue * 10) + 5;
     $('#displayNumber').text("Middle");
 });
 
-$('#six').click(function(){
+$('#Dexter').click(function(){
     $("#displayNumber").text("Right");
     numListValue = (numListValue * 10) + 6;
     $('#displayNumber').text("Right");

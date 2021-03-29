643ef5b34313b134662710c8f04cfea309693299
Kyle Barker <kb33@usca.edu>
2021-03-28 22:59:47 -0400

    test

--- Translator.js
+++ Translator.js
@@ -7,19 +7,16 @@ var numListValue = 0;
 
 $('#Sinister').click(function(){
     $("#displayNumber").text("Left");
-    numListValue = (numListValue * 10) + 4;
     $('#displayNumber').text("Left");
 });
 
 $('#Medium').click(function(){
     $("#displayNumber").text("Middle");
-    numListValue = (numListValue * 10) + 5;
     $('#displayNumber').text("Middle");
 });
 
 $('#Dexter').click(function(){
     $("#displayNumber").text("Right");
-    numListValue = (numListValue * 10) + 6;
     $('#displayNumber').text("Right");
 });
 

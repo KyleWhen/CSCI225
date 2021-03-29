c15676f99866ea3b4fa46b38e791261fc73588d6
Kyle Barker <kb33@usca.edu>
2021-03-28 23:07:49 -0400

    test

--- Translator.js
+++ Translator.js
@@ -36,38 +36,6 @@ $('#clear').click(function(){
     numListValue = "";
 });
 
-function docalculation(){
-    var result = 0;
-    var num2 = numList[numList.length-1];
-    console.log("num2="+num2);
-    numList.pop();
-    var num1 = numList[numList.length-1];
-    console.log("num1="+num1);
-    numList.pop();
-    oper = operList[operList.length-1];
-    console.log("oper="+oper);
-    operList.pop();
-    console.log("num1="+num1+" num2="+num2+" oper="+oper);
-    if (oper === '+'){
-        result = num1 + num2;
-        numList.push(result);
-    } else if (oper === '-'){
-        result = num1 - num2;
-        numList.push(result);
-    } else if (oper === '*') {
-        result = num1 * num2;
-        numList.push(result);
-    } else if (oper === '/') {
-        result = num1 / num2;
-        numList.push(result);
-    } else if (oper === '^') {
-        result = Math.pow(num1, num2);
-        numList.push(result);
-    }
-
-    $("#displayNumber").text(result);
-}
-
 $('#result').click(function(){
     numList.push(numListValue);
     numListValue = 0
